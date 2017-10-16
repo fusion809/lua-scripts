4c4412b76ea1a720b785d1c8db9f45069063ff88
fusion809 <fusion809@users.noreply.github.com>
2017-10-16 21:55:50 +1000

    Adding comments

--- binomial.lua
+++ binomial.lua
@@ -16,21 +16,28 @@
 --      Created:  16/10/17
 --     Revision:  ---
 --------------------------------------------------------------------------------
---
+-- Determines n choose k
+
+-- Get n
 local n
 io.write("Enter an integer value for n ")
 io.flush()
 n = io.read()
 
+-- Get k
 local k
 io.write("Enter an integer value for k ")
 io.flush()
 k = io.read()
 
+-- Initiate C
 local C = n
 
+-- Loop over i
 for i = 1,k-1 do
+    -- Calculate C for ith iteration
     C = (n-i)*C / (i+1)
 end
 
+-- C now equals the binomial coefficient
 print(C)
